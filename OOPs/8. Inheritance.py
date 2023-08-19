7d2d09f9ea29a3c5fc21f36789d1c55f8c322693
AryanShankar <aryanshankar325@gmail.com>
2023-08-20 03:45:39 +0530

    learnt some advanced concepts on oops

---
+++ OOPs/8. Inheritance.py
@@ -0,0 +1,22 @@
+class User():
+    def signed_in(self):
+        print("User is logged in.")
+        
+class Wizard(User):
+    def __init__(self, name, power):
+        self.name = name
+        self.power = power
+     
+    def attack(self):
+        print(f"{self.name} is attacking with {self.power} power.")
+        
+class Archer(User):
+    pass
+
+wizard1 = Wizard("John", 50)
+archer1 = Archer()
+
+print(wizard1.signed_in())
+print(wizard1.attack())
+
+print(archer1.signed_in())
---
+++ OOPs/9. isinstance.py
@@ -0,0 +1,27 @@
+'''
+By default every class is a subclass of 'object' class, hence when we type 'instance.' (object_name dot), we get all those
+defualt methods suggestions from the IDE.
+'''
+
+class User(object):   #is also as same as class User():
+    def signed_in(self):
+        print("User is logged in.")
+        
+class Wizard(User):
+    def __init__(self, name, power):
+        self.name = name
+        self.power = power
+     
+    def attack(self):
+        print(f"{self.name} is attacking with {self.power} power.")
+        
+class Archer(User):
+    pass
+
+wizard1 = Wizard("John", 50)
+
+print(isinstance(wizard1,Wizard))
+print(isinstance(wizard1,User))
+print(isinstance(wizard1,object)) 
+print(isinstance(wizard1,Archer))
+
